ab8893d97e7c89e8fac8b647fedc712d0450822a
Michael kang <michaelkang9898@gmail.com>
2021-09-21 03:02:16 +1200

    added endpoint

--- react-cseeker/src/App.js
+++ react-cseeker/src/App.js
@@ -9,7 +9,7 @@ function App() {
   const [jobs, setJobs] = useState([]);
 
   useEffect(() => {
-    fetch('/jobs').then(
+    fetch('http://54.153.139.15/jobs').then(
       response => response.json().then(data => {
         setJobs(data);
       })
